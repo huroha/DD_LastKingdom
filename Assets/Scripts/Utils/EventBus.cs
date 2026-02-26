797de16ae4de523322066734f2994b29887fde0a
huroha <kmsms4050@gmail.com>
2026-02-26 16:27:22 +0900

    Normalize line ending

--- Assets/Scripts/Utils/EventBus.cs
+++ Assets/Scripts/Utils/EventBus.cs
@@ -9,35 +9,44 @@ using System.Collections.Generic;
 /// </summary>
 public static class EventBus
 {
-    private static readonly Dictionary<Type, Delegate> _handlers = new();
+    private static readonly Dictionary<Type, List<Delegate>> _handlers = new Dictionary<Type, List<Delegate>>();
+
+    // TEvent는 하나의 큰 이벤트의 종류 ex) PlayerDiedEvent
+    // 추가적으로 딕셔너리로 delegate로 관련된 하위 함수 이벤트들을 가지고있는다. ex ) OnPlayerDied_UI, OnPlayerDied_Sound 등등
+    // Dictionary로 만든 이유 -> 검색에서 O(1)의 속도를 보여준다. list는 전체 리스트를 확인하기 때문에 O(n)
+    // Delegate는 함수를 변수로 선언한것.
 
     public static void Subscribe<TEvent>(Action<TEvent> handler)
     {
-        var type = typeof(TEvent);
-        if (_handlers.TryGetValue(type, out var existing))
-            _handlers[type] = Delegate.Combine(existing, handler);
-        else
-            _handlers[type] = handler;
-    }
-
+        Type type = typeof(TEvent);                                        
+        if (!_handlers.ContainsKey(type))                                   
+            _handlers[type] = new List<Delegate>();                        
+        _handlers[type].Add(handler);                                      
+    }                                                                      
+                                                                           
     public static void Unsubscribe<TEvent>(Action<TEvent> handler)
     {
-        var type = typeof(TEvent);
-        if (_handlers.TryGetValue(type, out var existing))
-        {
-            var updated = Delegate.Remove(existing, handler);
-            if (updated == null)
-                _handlers.Remove(type);
-            else
-                _handlers[type] = updated;
-        }
+        Type type = typeof(TEvent);
+        if (!_handlers.ContainsKey(type))
+            return;
+        
+        _handlers[type].Remove(handler);
+        if (_handlers[type].Count == 0)
+            _handlers.Remove(type);
+       
+       
     }
 
     public static void Publish<TEvent>(TEvent evt)
     {
-        var type = typeof(TEvent);
-        if (_handlers.TryGetValue(type, out var handler))
-            ((Action<TEvent>)handler).Invoke(evt);
+        Type type = typeof(TEvent);
+        if (!_handlers.ContainsKey(type))
+            return;
+        List<Delegate> handlers = new List<Delegate>(_handlers[type]);      // 복사본을 순회
+        for(int i = 0; i < handlers.Count; ++i)
+        {
+            ((Action<TEvent>)handlers[i]).Invoke(evt);  
+        }
     }
 
     public static void Clear()
